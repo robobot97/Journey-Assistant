ca97842eb7a88bc049e9b548d7305406f16345ad
Arjun Khurana <arjunkhurana26@hotmail.com>
2019-01-29 23:37:31 +0000

    general plan of action

---
+++ journeyAssistant.py
@@ -0,0 +1,7 @@
+"""
+steps:
+1. get data regarding to tubes from tfl
+2. get data regarding schedule from (google) calendar
+3. for each day of the week, find the most likely journey based on schedule
+4. display necessary info regarding this journey
+
